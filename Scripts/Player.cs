785432cd94c2e08db4072d83193705a4079ec820
Shinjoku <deora.2@hotmail.com>
2023-10-27 01:05:14 -0300

    feat: add player animation and individual scenes

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,32 +2,56 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 100.0f;
+	public const float Speed = 80.0f;
 	public const float JumpVelocity = -400.0f;
+	private AnimationTree _animationTree = null;
+	private AnimationNodeStateMachinePlayback _animationState = null;
+
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+        _animationTree = GetNode<AnimationTree>(nameof(AnimationTree));
+        _animationState = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		// if (!IsOnFloor())
-		// 	velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// 	velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
 		Velocity = direction != Vector2.Zero ?
-			direction * Speed :
-			velocity.MoveToward(Vector2.Zero, Speed);
+			Walk(direction) : Stop(velocity);
 
 		MoveAndSlide();
 	}
+
+	public Vector2 Walk(Vector2 direction)
+	{
+		SetAnimationTree("Idle", direction);
+		SetAnimationTree("Run", direction);
+		_animationState.Travel("Run");
+		return direction * Speed;
+	}
+	
+	public Vector2 Stop(Vector2 velocity)
+	{
+		_animationState.Travel("Idle");
+		return velocity.MoveToward(Vector2.Zero, Speed);
+	}
+
+	private void SetAnimationTree(string param, Vector2 vector) =>
+		_animationTree.Set($"parameters/{param}/blend_position", vector);
+
+	// Commented code:
+	// Add the gravity.
+	// if (!IsOnFloor())
+	// 	velocity.Y += gravity * (float)delta;
+
+	// Handle Jump.
+	// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+	// 	velocity.Y = JumpVelocity;
 }
