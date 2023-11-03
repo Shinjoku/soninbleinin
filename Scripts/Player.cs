e89138b5f4505d68ed5aa39145f9d3f691c4f29c
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-11-03 01:17:22 -0300

    feat: added attack animation
    Pressing space or J will now trigger attack animation

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -6,13 +6,21 @@ enum AnimationTreeParameters
     BlendPosition
 }
 
+enum PlayerState
+{
+    Move,
+    Roll,
+    Attack
+}
+
 public partial class Player : CharacterBody2D
 {
     public const float Speed = 80.0f;
     public const float JumpVelocity = -400.0f;
     private AnimationTree _animationTree = null;
     private AnimationNodeStateMachinePlayback _animationState = null;
-
+    private AnimationPlayer _animationPlayer = null;
+    private PlayerState _playerState = PlayerState.Move;
 
     // Get the gravity from the project settings to be synced with RigidBody nodes.
     public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -21,24 +29,24 @@ public partial class Player : CharacterBody2D
     {
         _animationTree = GetNode<AnimationTree>(nameof(AnimationTree));
         _animationState = (AnimationNodeStateMachinePlayback)_animationTree.Get("parameters/playback");
+        _animationPlayer = GetNode<AnimationPlayer>(nameof(AnimationPlayer));
+
+        _animationTree.Active = true;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 velocity = Velocity;
-
-        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-
-        Velocity = direction != Vector2.Zero ?
-            Walk(direction) : Stop(velocity);
-
-        MoveAndSlide();
+        if (_playerState == PlayerState.Move)
+            MoveState(delta);
+        else if (_playerState == PlayerState.Attack)
+            AttackState(delta);
     }
 
     public Vector2 Walk(Vector2 direction)
     {
         SetAnimationTree("Idle", direction);
         SetAnimationTree("Run", direction);
+        SetAnimationTree("Attack", direction);
         _animationState.Travel("Run");
         return direction * Speed;
     }
@@ -49,6 +57,11 @@ public partial class Player : CharacterBody2D
         return velocity.MoveToward(Vector2.Zero, Speed);
     }
 
+    public void AttackAnimationFinished()
+    {
+        _playerState = PlayerState.Move;
+    }
+
     private void SetAnimationTree(string animation, Vector2 vector, AnimationTreeParameters param = AnimationTreeParameters.BlendPosition) =>
         _animationTree.Set($"parameters/{animation}/{GetParameterString(param)}", vector);
 
@@ -57,6 +70,26 @@ public partial class Player : CharacterBody2D
         AnimationTreeParameters.BlendPosition => "blend_position",
         _ => throw new NotImplementedException()
     };
+
+    private void MoveState(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+        Velocity = direction != Vector2.Zero ?
+            Walk(direction) : Stop(velocity);
+
+        MoveAndSlide();
+
+        if (Input.IsActionJustPressed("attack"))
+            _playerState = PlayerState.Attack;
+    }
+
+    private void AttackState(double delta)
+    {
+        _animationState.Travel("Attack");
+    }
 
     // Commented code:
     // Add the gravity.
