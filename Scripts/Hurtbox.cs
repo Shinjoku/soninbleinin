9590851c27608280b584548515d0e3b0161f79a4
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-11-08 22:14:26 -0300

    feat: hurtbox hit effect is now optional
    Hurtbox hit effect is now optional, and by default hurtboxes will show the effect

--- Scripts/Hurtbox.cs
+++ Scripts/Hurtbox.cs
@@ -3,9 +3,16 @@ using System;
 
 public partial class Hurtbox : Area2D
 {
+	[Export]
+	private bool ShowHitEffect = true;
+
 	private PackedScene HitEffectScene = ResourceLoader.Load<PackedScene>("res://Scenes/HitEffect.tscn");
 
-	public void _OnAreaEntered(Area2D body) {
+	public void _OnAreaEntered(Area2D body)
+	{
+		if (!ShowHitEffect)
+			return;
+
 		var effect = HitEffectScene.Instantiate<Effect>();
 		effect.GlobalPosition = GlobalPosition;
 		GetTree().CurrentScene.AddChild(effect);
